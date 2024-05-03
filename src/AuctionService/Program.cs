e02a10b5349033926e9493467511f2d8a89d0dab
matan Morduch <matanmorduch@matans-MacBook-Pro.local>
2024-05-03 22:19:52 +0300

    end section 4

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,6 @@
+using AuctionService;
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,26 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o => {
+        // Attempt to look in outbox and deliver the message
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+        // Configure to use postgres
+        o.UsePostgres();
+        // Configure to use Bus Outbox technique.
+        o.UseBusOutbox();
+    });
+
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,9 +1,12 @@
 using System.Net;
+using Amazon.Runtime.Internal.Auth;
+using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService;
+using SearchService.Consumers;
 using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,9 +14,32 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Register all the available AutoMapper profiles defined within my project
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Register the http client to get the data from the Auction SVC 
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+// Register the RabbitMQ service
+builder.Services.AddMassTransit(x =>
+{
+    // Scan the assembly containing the 'AuctionCreatedConsumer' class and register any consumer
+    // classes found within the same namespace
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    // Add the word search at the start of each Exchange
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    // configures MassTransit to use RabbitMQ as the message transport
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ReceiveEndpoint("search-auction-created", e => {
+            e.UseMessageRetry(r => r.Interval(5,5));
+
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+        // configures MassTransit endpoints for RabbitMQ
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
